46ba0fbcace89fe7c85d8ac5a5dfbfb4d5f4c662
sethjuarez <me@sethjuarez.com>
2015-01-22 16:39:22 -0800

    beggining work on query xml doc

---
+++ FastDoc.Core.Tests/XmlDocTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDoc.Core.Tests
+{
+    [TestFixture]
+    public class XmlDocTests
+    {
+        [Test]
+        public void RetrieveMethod()
+        {
+            XmlDoc doc = new XmlDoc("numl.xml");
+            
+        }
+    }
+}
---
+++ FastDoc.Core/XmlDoc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDoc.Core
+{
+    public class XmlDoc
+    {
+        public string File { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the XmlDoc class.
+        /// </summary>
+        /// <param name="file"></param>
+        public XmlDoc(string file)
+        {
+            File = file;
+        }
+
+        public string GetDocumentation(ItemType type, string path)
+        {
+            return "";
+        }
+
+        public string GetDocumentation(MemberType type, string path)
+        {
+            return "";
+        }
+    }
+}
